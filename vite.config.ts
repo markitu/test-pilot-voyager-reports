889576b7ec3e0566e7c3410c0494147b6a3eb6e1
Igor Mashkov <imashkov@vtb.ru>
2025-05-30 13:35:33 +0300

    fixed admiral import errors

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,11 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      external: [
+        /@admiral-ds\/fonts\/.*\.ttf$/,
+      ],
+    },
+  },
 }));
